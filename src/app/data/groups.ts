ec8786a5aeb769be73a1843e3536685dd41678a0
Linus Herterich <linus.herterich@gmail.com>
2025-05-21 11:17:54 +0200

    Changed Groups

--- src/app/data/groups.ts
+++ src/app/data/groups.ts
@@ -1,8 +1,8 @@
 import { Group } from '../model/data';
 
 export const groups: Group[] = [
-  { groupName: 'Gruppe 1', members: ['Philipp', 'Lena Sartorius', 'Sebbo', 'Franzi', 'Clara'], points: 0 },
-  { groupName: 'Gruppe 2', members: ['Lena Sturz', 'Pit', 'Thao', 'Lucas', 'Luise'], points: 0 },
-  { groupName: 'Gruppe 3', members: ['Jonas', 'Andrès', 'Alice', 'Silja', 'Jannik'], points: 0 },
-  { groupName: 'Gruppe 4', members: ['Anika', 'Feli','Caro', 'Simon', 'Alina'], points: 0 }
+  { groupName: 'Gruppe 1', members: ['Svenja', 'Lena Sartorius', 'Gideon', 'Franzi', 'Sophie'], points: 0 },
+  { groupName: 'Gruppe 2', members: ['Thao', 'Pearl', 'Lucas', 'Luise', 'Philipp'], points: 0 },
+  { groupName: 'Gruppe 3', members: ['Jonas', 'Andrès', 'Simon', 'Lukas', 'Rica'], points: 0 },
+  { groupName: 'Gruppe 4', members: ['Eric', 'Lilli','Luisa', 'Alex', 'Richard', 'Lisa'], points: 0 }
 ];
